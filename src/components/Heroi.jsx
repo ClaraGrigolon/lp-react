df570a061d4bc65ee2fabe5f997f99e346033c5f
ClaraGrigolon <claragrigolon@gmail.com>
2025-03-13 12:33:25 -0300

    Add files via upload

---
+++ src/components/Heroi.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Heroi.css'
+
+function Heroi(){
+    return(
+        <>
+            <section id="hero" className="hero">
+                <div className="hero-content">
+                    <h2>Entre no mundo de Valorant</h2>
+                    <p>Experimente a emoção e estratégia deste incrível universo.</p>
+                    <button>Saiba mais</button>
+                </div>
+            </section>
+        </>
+    );
+};
+export default Header;
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import Header from './components/Header.jsx'
+import Heroi from './components/Heroi.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Header />
+    <Heroi />
+  </StrictMode>,
+)
